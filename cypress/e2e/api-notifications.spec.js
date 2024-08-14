37e85c59d780b83d6fd1f9ebdbcb4c51137516ad
Barthélémy Ledoux <bart@cypress.io>
2024-08-14 17:46:40 +0200

    add a test to see if it does something

--- cypress/e2e/api-notifications.spec.js
+++ cypress/e2e/api-notifications.spec.js
@@ -1,6 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe("empty spec", () => {
+  it("passes", () => {
     // throw Error("arbitrary error");
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit("https://example.cypress.io");
+  });
+
+  it("passes too", () => {
+    // throw Error("arbitrary error");
+    cy.visit("https://example.cypress.io");
+  });
+});
